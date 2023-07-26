8101d4d37115cc72064462c71fb70e4d11a48ef7
Swati Pandey <swati.p@prograd.org>
2023-07-26 15:23:13 +0530

    Add App Class Component in exercise-lab-jsx-boilerplate

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App.js'
+// import App from './App.js'
+import AppClass from './AppClass';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  // <App />
+  <AppClass />
 );
